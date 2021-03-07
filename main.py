5711c01204c280407eada96e5b45e78956e2d42d
Bora Yüret <bora.yuret@bilgeadam.com>
2021-03-07 13:12:08 +0300

    label'lar eklendi.

---
+++ main.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+# ---------- fonksiyon bölümü ----------
+
+def center_window(w=300, h=200):
+    # get screen width and height
+    ws = window.winfo_screenwidth()
+    hs = window.winfo_screenheight()
+    # calculate position x, y
+    x = (ws/2) - (w/2)
+    y = (hs/2) - (h/2)
+    window.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+
+# ---------- UI Bölümü ------------------
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+
+# Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+
+
+
+center_window(400,400)
+window.mainloop()
