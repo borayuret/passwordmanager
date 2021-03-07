67e2b05c51071d64b031942cd1784feeb5d04d98
Bora Yüret <bora.yuret@bilgeadam.com>
2021-03-07 14:23:26 +0300

    şifreyi save eder hale getirdik.

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from tkinter import *
+from tkinter import messagebox
 
 # ---------- fonksiyon bölümü ----------
+
+def save():
+    website = website_text.get()
+    email = email_text.get()
+    password = password_text.get()
+
+    with open('data.txt', 'a') as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+
+    messagebox.showinfo(title=website, message="Your info is saved successfully!")
+
+    clear()
+
+def clear():
+    website_text.set("")
+    email_text.set("")
+    password_text.set("")
 
 def center_window(w=300, h=200):
     # get screen width and height
@@ -14,24 +32,41 @@ def center_window(w=300, h=200):
 
 # ---------- UI Bölümü ------------------
 window = Tk()
-window.title("Password Manager")
+window.title("Password Manager - v1.0")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(row=0, column=1)
+canvas.grid(row=0, column=1, columnspan=3)
 
 # Labels
 website_label = Label(text="Website:")
-website_label.grid(row=1, column=0)
+website_label.grid(row=1, column=0, sticky=E)
 email_label = Label(text="Email/Username:")
-email_label.grid(row=2, column=0)
+email_label.grid(row=2, column=0, sticky=E)
 password_label = Label(text="Password:")
-password_label.grid(row=3, column=0)
+password_label.grid(row=3, column=0, sticky=E)
+
+# Entrys
+website_text = StringVar();
+website_entry = Entry(width=52, textvariable=website_text)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+email_text = StringVar();
+email_entry = Entry(width=52, textvariable=email_text)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_text.set("borayuret@gmail.com")
+password_text = StringVar();
+password_entry = Entry(width=33, textvariable=password_text, show='*')
+password_entry.grid(row=3, column=1)
+
+# Buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=44, command=save)
+add_button.grid(row=4, column=1, columnspan=2)
 
 
-
-
-center_window(400,400)
+center_window(550,400)
 window.mainloop()
